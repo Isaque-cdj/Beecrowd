6cd7b8c8c9953a47044fd57fe674d552fe574a7c
Isaque Coelho <isaquecoelho.dj@gmail.com>
2024-12-20 15:13:14 -0300

    bee1040, primeira tentativa

--- src-c/bee1040.c
+++ src-c/bee1040.c
@@ -1,7 +1,64 @@
-#include <stdio.h>
- 
-int main() {
+/*
+Leia quatro números (N1, N2, N3, N4), cada um deles com uma casa decimal, correspondente às quatro notas de um aluno. Calcule a média com
+pesos 2, 3, 4 e 1, respectivamente, para cada uma destas notas e mostre esta média acompanhada pela mensagem "Media: ". Se esta média for
+maior ou igual a 7.0, imprima a mensagem "Aluno aprovado.". Se a média calculada for inferior a 5.0, imprima a mensagem "Aluno reprovado.".
+Se a média calculada for um valor entre 5.0 e 6.9, inclusive estas, o programa deve imprimir a mensagem "Aluno em exame.".
 
-   
+No caso do aluno estar em exame, leia um valor correspondente à nota do exame obtida pelo aluno. Imprima então a mensagem "Nota do exame:
+" acompanhada pela nota digitada. Recalcule a média (some a pontuação do exame com a média anteriormente calculada e divida por 2).
+Imprima a mensagem "Aluno aprovado." (caso a média final seja 5.0 ou mais) ou "Aluno reprovado." (caso a média tenha ficado 4.9 ou menos).
+Para estes dois casos (aprovado ou reprovado após ter pego exame), apresente na última linha uma mensagem "Media final: " seguido da média
+final para esse aluno.
+*/
+
+#include <stdio.h>
+// notas
+float n1, n2, n3, n4;
+// pesos
+int p1 = 2, p2 = 3, p3 = 4, p4 = 1;
+
+float media;
+
+int main()
+{
+
+    printf("Digite as notas\n");
+    scanf("%f", &n1);
+    printf("Digite 2° a nota\n");
+    scanf("%f", &n2);
+    printf("Digite 3° a nota\n");
+    scanf("%f", &n3);
+    printf("Digite 4° a nota\n");
+    scanf("%f", &n4);
+
+    media = ((p1 * n1) + (p2 * n2) + (p3 * n3) + (p4 * n4)) / (p1 + p2 + p3 + p4);
+    printf("Media: %.1f\n", media);
+
+    if (media >= 7)
+    {
+        printf("Aluno aprovado.");
+    }
+    else if (media < 5)
+    {
+        printf("Aluno reprovado.");
+    }
+    else if (media >= 5 && media < 7)
+    {
+        printf("Aluno em exame.\n");
+        float notaExam;
+        scanf("%f", &notaExam);
+        float mediaFinal = (media + notaExam) / 2;
+        if (mediaFinal >= 5)
+        {
+            printf("Aluno aprovado.\n");
+            printf("Media final: ", mediaFinal);
+        }
+        else
+        {
+            printf("Aluno reprovado.\n", mediaFinal);
+            printf("Media final: ", mediaFinal);
+        }
+    }
+
     return 0;
 }
